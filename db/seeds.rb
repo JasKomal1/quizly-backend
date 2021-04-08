fc9ece41b802636ed308e418f4a52d446b980538
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-04-08 14:08:35 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(name: 'jsr')
+
+quiz1 = Quiz.create(name: 'famous people', category: 'People')
+quiz2 = Quiz.create(name: 'sports teams', category: 'Sports')
+quiz3 = Quiz.create(name: 'political parties', category: 'Politics')
+
+question1 = Question.create(title:'Who was the first president', choice1:'Lincoln', choice2: 'Jefferson', choice3: 'Washington', answer: 'Washington', quiz_id: quiz1.id)
+question2 = Question.create(title:'LA baseball team', choice1:'Angels', choice2: 'Dodgers', choice3: 'Padres', answer: 'Dodgers', quiz_id: quiz2.id)
+question3 = Question.create(title:'Who is a democrat', choice1:'Ted Cruz', choice2: 'Gavin Newsom', choice3: 'Bernie Sanders', answer: 'Gavin Newsom', quiz_id: quiz3.id)
+
+fave1 = Favorite.create(user_id: u1.id, quiz_id: quiz1.id)
+fave2 = Favorite.create(user_id: u1.id, quiz_id: quiz2.id)
