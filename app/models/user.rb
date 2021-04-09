76994dadf4feb61cf4e0c46eebfe1423f24994e5
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-04-09 12:38:36 -0500

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :userquizzes
+    has_many :quizzes, through: :userquizzes
 end
--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -1,3 +1,4 @@
 class QuizSerializer < ActiveModel::Serializer
   attributes :id, :name, :category
+  has_many :questions 
 end
